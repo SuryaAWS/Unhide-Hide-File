35e4b4a4c0c0e306c39dcbcf799eea61024068c8
SuryaAWS <suryanaolin@gmail.com>
2017-07-20 03:55:00 +0700

    v1.0

---
+++ Hide File.cmd
@@ -0,0 +1,15 @@
+@echo off
+color 0A
+cls
+
+title Hide File
+
+echo Silahkan tunggu . . . .
+
+attrib +s +h +r +a /s /d
+
+cls
+
+echo File-filenya sudah terhidden semua . . . .
+
+pause
---
+++ Unhide File.cmd
@@ -0,0 +1,15 @@
+@echo off
+color 0A
+cls
+
+title Unhide File
+
+echo Silahkan tunggu . . . .
+
+attrib -a -r -s -h /s /d
+
+cls
+
+echo File-filenya sudah terlihat semua . . . .
+
+pause
